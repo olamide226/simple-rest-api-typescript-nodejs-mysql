cafbe71eb719b5ad8138139921114e307f74cad1
Ola Adebayo <olamideadebayo2001@gmail.com>
2023-06-05 18:20:02 +0100

    update packages

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,14 @@
 import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export async function connect() {
 
     const connection = await createPool({
-        host: 'localhost',
-        user: 'root',
-        password: '123456',
-        database: 'demo_all',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         connectionLimit: 10
     });
     
